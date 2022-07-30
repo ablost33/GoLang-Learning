93aec7791e60b24a415b7cd4cebae4d64fa56917
Alex <Blostein>
2022-07-30 16:24:06 -0400

    init

---
+++ ch11/word_test.go
@@ -0,0 +1,22 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+//!+test
+package word
+
+import "testing"
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome") {
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
---
+++ ch4/charcount.go
@@ -0,0 +1,35 @@
+// This program counts the occurrences of each distinc Unicode code point in its input
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make([rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprint(os.Stderr, "charcount: %v\n", err)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+}
