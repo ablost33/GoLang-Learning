9f665699f0d9d4f090d2603aeb9dfffb8d151d94
Alex <Blostein>
2022-08-21 16:54:52 -0400

    08/21/2022

---
+++ ch5/findlinks1.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"./html"
+	"fmt"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+/*
+	This function traverses the HTML node tree, extracts link from the href attribute of each anchor element and appends
+	the links to a slice of strings, and returns the resulting slice
+*/
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirsChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
+
+/*
+	This function recurses over the HTML node tree to print the structure of the tree in
+	the outline
+*/
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirsChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}
---
+++ ch5/findlinks2.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"./html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
+}
+
+/*
+	This function does an HTTP GET request for the HTML document url and returns the number of
+	words and images in it
+*/
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = CountWordsAndImages(doc)
+	return
+}
