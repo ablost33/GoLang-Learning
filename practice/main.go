ee0acfbdcd0e217da9e0f42b906391d80b90ce7f
Alex <Blostein>
2024-01-03 00:31:59 -0500

    Katas 01/03

--- practice/main.go
+++ practice/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ablost33/GoLang-Learning/practice/transaction"
+	"github.com/ablost33/GoLang-Learning/practice/http_practice"
 )
 
 /* @TODO: Answer the following
@@ -26,5 +26,5 @@ interdependencies, do: go run main.go arrays_and_slices.go
 */
 
 func main() {
-	transaction.LoadAndAnalyze()
+	http_practice.MakeGetRequest()
 }
