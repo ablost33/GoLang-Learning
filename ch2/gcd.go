6c2b1f13a9096b65170391a7b8280bfd3c8a9bee
Alex <Blostein>
2024-01-03 23:40:14 -0500

    Restructured folders

--- ch2/gcd.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func gcd(x, y int) int {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
-}
--- ch4/reverse.go
+++
@@ -1,7 +0,0 @@
-package ch4
-
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
