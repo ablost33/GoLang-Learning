0bb89b2caf9d954d961cd626f24bb636a0df24c3
Alex <Blostein>
2024-04-23 00:10:09 -0400

    Practice 04/22

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	api_authentication "github.com/ablost33/GoLang-Learning/random_exercises/api-authentication"
+	"github.com/ablost33/GoLang-Learning/random_exercises/csv_n_post"
 )
 
 func main() {
-	in := api_authentication.GetCatRequest{}
-	api_authentication.GetCatImages(in)
+	csv_n_post.CsvParsing2()
 }
